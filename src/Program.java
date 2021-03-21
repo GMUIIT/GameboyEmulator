2125ba802ef4b2487d3cbfe89e9cb61baeda0bde
Terutsa97 <51282156+Terutsa97@users.noreply.github.com>
2021-03-21 12:12:00 -0400

    Update and rename program.java to Program.java

--- src/Program.java
+++ src/Program.java
@@ -3,14 +3,17 @@
 
 /**
  * This is a tester program. (Edited by Angel)
+ * 
+ * If you want to compile and run this tester, open the src folder in the terminal.
+ * 
  * You compile this with this command:
- * javac -d compiled program.java
+ * javac -d compiled Program.java
  * 
  * run with this command:
- * java -cp compiled program
+ * java -cp compiled Program
  */
  
-public class program {
+public class Program {
   public static void main(String[] args) {
     // Put tester code here!
     CPU cpu = new CPU();
