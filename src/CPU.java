6d88275e2af048178c88f1ab4870d84f831407b4
Terutsa97 <51282156+Terutsa97@users.noreply.github.com>
2021-04-17 13:40:28 -0400

    Added decoded skeleton code for Immediate ALU instructions, Rotation instructions, Extended functions, z=7 functions for x=0, and more in the CPU.

--- src/CPU.java
+++ src/CPU.java
@@ -75,6 +75,17 @@ public class CPU {
     Rot_t() { this.index = this.ordinal(); }
   }
 
+  /**
+   * 
+   */
+  enum Z7_t {
+    RLCA, RRCA, RLA, RRA, DAA, CPL, SCF, CCF;
+
+    public int index;
+
+    Z7_t() { this.index = this.ordinal(); }
+  }
+
   // Opcode Argument Types
   final Reg_8[] r_args = { Reg_8.B, Reg_8.C, Reg_8.D, Reg_8.E, Reg_8.H, Reg_8.L, null, Reg_8.A };
   final Reg_16[] rp_args = { Reg_16.BC, Reg_16.DE, Reg_16.HL, Reg_16.SP };
@@ -82,11 +93,18 @@ public class CPU {
   final CC_t[] cc_args = CC_t.values();
   final Alu_t[] alu_args = Alu_t.values();
   final Rot_t[] rot_args = Rot_t.values();
+  final Z7_t[] z7_args = Z7_t.values();
 
   @FunctionalInterface
-  interface AluCommand { public void invoke(Reg_8 source); }
-  
-  HashMap<Alu_t, AluCommand> AluMap = new HashMap<Alu_t, AluCommand>();
+  interface func { public void invoke(); }
+
+  @FunctionalInterface
+  interface func_reg8 { public void invoke(Reg_8 source); }
+
+  HashMap<Alu_t, func_reg8> AluMap = new HashMap<Alu_t, func_reg8>();
+  HashMap<Alu_t, func> AluImMap = new HashMap<Alu_t, func>();
+  HashMap<Z7_t, func> Z7Map = new HashMap<Z7_t, func>();
+  HashMap<Rot_t, func_reg8> RotMap = new HashMap<Rot_t, func_reg8>();
 
   public void initializeHashmaps() {
     AluMap.put(Alu_t.ADC, (source) -> ADC(source));
@@ -97,6 +115,33 @@ public class CPU {
     AluMap.put(Alu_t.SBC, (source) -> SBC(source));
     AluMap.put(Alu_t.SUB, (source) -> SUB(source));
     AluMap.put(Alu_t.XOR, (source) -> XOR(source));
+
+    AluImMap.put(Alu_t.ADC, () -> ADC_I());
+    AluImMap.put(Alu_t.ADD, () -> ADD_I());
+    AluImMap.put(Alu_t.AND, () -> AND_I());
+    AluImMap.put(Alu_t.CP,  () -> CP_I());
+    AluImMap.put(Alu_t.OR,  () -> OR_I());
+    AluImMap.put(Alu_t.SBC, () -> SBC_I());
+    AluImMap.put(Alu_t.SUB, () -> SUB_I());
+    AluImMap.put(Alu_t.XOR, () -> XOR_I());
+
+    Z7Map.put(Z7_t.RLCA, () -> RLCA());
+    Z7Map.put(Z7_t.RRCA, () -> RRCA());
+    Z7Map.put(Z7_t.RLA,  () -> RLA());
+    Z7Map.put(Z7_t.RRA,  () -> RRA());
+    Z7Map.put(Z7_t.DAA,  () -> DAA());
+    Z7Map.put(Z7_t.CPL,  () -> CPL());
+    Z7Map.put(Z7_t.SCF,  () -> SCF());
+    Z7Map.put(Z7_t.CCF,  () -> CCF());
+
+    RotMap.put(Rot_t.RLC,  (source) -> RLC(source));
+    RotMap.put(Rot_t.RRC,  (source) -> RRC(source));
+    RotMap.put(Rot_t.RL,   (source) -> RL(source));
+    RotMap.put(Rot_t.RR,   (source) -> RR(source));
+    RotMap.put(Rot_t.SLA,  (source) -> SLA(source));
+    RotMap.put(Rot_t.SRA,  (source) -> SRA(source));
+    RotMap.put(Rot_t.SWAP, (source) -> SWAP(source));
+    RotMap.put(Rot_t.SRL,  (source) -> SRL(source));
   }
 
 //#endregion
@@ -161,6 +206,7 @@ public class CPU {
   //#region ---- ---- ---- ---- ---- ALU Opcodes
   /**
    * 
+   * @param source
    */
   void ADC(Reg_8 source) {
     int value = (source != null) ? regSet.getByte(source) : regSet.getWord(Reg_16.HL);
@@ -177,7 +223,6 @@ public class CPU {
 
   /**
    * 
-   * @param destination
    * @param source
    */
   void ADD(Reg_8 source) {
@@ -221,6 +266,7 @@ public class CPU {
 
   /**
    * 
+   * @param source
    */
   void SUB(Reg_8 source) {
     int value = (source != null) ? regSet.getByte(source) : regSet.getWord(Reg_16.HL);
@@ -237,6 +283,7 @@ public class CPU {
 
   /**
    * 
+   * @param source
    */
   void SBC(Reg_8 source) {
     int value = (source != null) ? regSet.getByte(source) : regSet.getWord(Reg_16.HL);
@@ -253,6 +300,7 @@ public class CPU {
   
   /**
    * 
+   * @param source
    */
   void AND(Reg_8 source) {
     int value = (source != null) ? regSet.getByte(source) : regSet.getWord(Reg_16.HL);
@@ -269,6 +317,7 @@ public class CPU {
 
   /**
    * 
+   * @param source
    */
   void OR(Reg_8 source) {
     int value = (source != null) ? regSet.getByte(source) : regSet.getWord(Reg_16.HL);
@@ -285,6 +334,7 @@ public class CPU {
 
   /**
    * 
+   * @param source
    */
   void XOR(Reg_8 source) {
     int value = (source != null) ? regSet.getByte(source) : regSet.getWord(Reg_16.HL);
@@ -301,6 +351,7 @@ public class CPU {
 
   /**
    * 
+   * @param source
    */
   void CP(Reg_8 source) {
     int value = (source != null) ? regSet.getByte(source) : regSet.getWord(Reg_16.HL);
@@ -317,15 +368,33 @@ public class CPU {
 
   /**
    * 
+   * @param source
    */
-  void INC() {
+  void INC(Reg_8 source) {
+    
+  }
+
+  /**
+   * 
+   * @param source
+   */
+  void INC_16(Reg_16 source) {
+    
+  }
+
+  /**
+   * 
+   * @param source
+   */
+  void DEC(Reg_8 source) {
 
   }
 
   /**
    * 
+   * @param source
    */
-  void DEC() {
+  void DEC_16(Reg_16 source) {
 
   }
 
@@ -335,9 +404,11 @@ public class CPU {
 
   /**
    * 
+   * @param source
    */
-  void SWAP() {
-
+  void SWAP(Reg_8 source) {
+    int value = (source != null) ? regSet.getByte(source) : regSet.getWord(Reg_16.HL);
+    
   }
 
   /**
@@ -437,50 +508,64 @@ public class CPU {
 
   /**
    * 
+   * @param source
    */
-  void RLC() {
+  void RLC(Reg_8 source) {
+    int value = (source != null) ? regSet.getByte(source) : regSet.getWord(Reg_16.HL);
 
   }
   
   /**
    * 
+   * @param source
    */
-  void RL() {
+  void RL(Reg_8 source) {
+    int value = (source != null) ? regSet.getByte(source) : regSet.getWord(Reg_16.HL);
+    
+  }
+  
+  /**
+   * 
+   * @param source
+   */
+  void RRC(Reg_8 source) {
+    int value = (source != null) ? regSet.getByte(source) : regSet.getWord(Reg_16.HL);
 
   }
   
   /**
    * 
+   * @param source
    */
-  void RRC() {
+  void RR(Reg_8 source) {
+    int value = (source != null) ? regSet.getByte(source) : regSet.getWord(Reg_16.HL);
+
+  }
+
+  /**
+   * 
+   * @param source
+   */
+  void SLA(Reg_8 source) {
+    int value = (source != null) ? regSet.getByte(source) : regSet.getWord(Reg_16.HL);
 
   }
   
   /**
    * 
+   * @param source
    */
-  void RR() {
-
-  }
-
-  /**
-   * 
-   */
-  void SLA() {
+  void SRA(Reg_8 source) {
+    int value = (source != null) ? regSet.getByte(source) : regSet.getWord(Reg_16.HL);
 
   }
   
   /**
    * 
+   * @param source
    */
-  void SRA() {
-
-  }
-  
-  /**
-   * 
-   */
-  void SRL() {
+  void SRL(Reg_8 source) {
+    int value = (source != null) ? regSet.getByte(source) : regSet.getWord(Reg_16.HL);
 
   }
 
@@ -490,22 +575,28 @@ public class CPU {
 
   /**
    * 
+   * @param b
+   * @param source
    */
-  void BIT() {
+  void BIT(int b, Reg_8 source) {
 
   }
 
   /**
    * 
+   * @param b
+   * @param source
    */
-  void SET() {
+  void SET(int b, Reg_8 source) {
 
   }
 
   /**
    * 
+   * @param b
+   * @param source
    */
-  void RES() {
+  void RES(int b, Reg_8 source) {
 
   }
 
@@ -593,7 +684,7 @@ public class CPU {
   public int executeOpcode(short opcode) {
 
     // CB-prefixed opcodes
-    if (opcode == 0xCB) { executeExtendedOpcode(); }
+    if (opcode == 0xCB) { return executeExtendedOpcode(); }
 
     int x_arg = (opcode & X_MASK) >> 6;
     int y_arg = (opcode & Y_MASK) >> 3;
@@ -603,7 +694,39 @@ public class CPU {
 
     switch(x_arg) {
       case 0:
-        // Opcodes
+        switch(z_arg) {
+          case 0:
+            // NOP
+            // LD (nn),SP
+            // STOP
+            // JR d
+            // JR cc[y-4],d
+            break;
+          case 1:
+            // Q = 0, LD rp[p],nn
+            // Q = 1, ADD HL, rp[p]
+            break;
+          case 2:
+            // Q = 0, LD (??),A
+            // Q = 1, LD A,(??)
+            break;
+          case 3:
+            // Q = 0  INC rp[p]
+            // Q = 1  DEC rp[p]
+            break;
+          case 4:
+            INC(r_args[y_arg]);
+            break;
+          case 5:
+            DEC(r_args[y_arg]);
+            break;
+          case 6:
+            // LD r[y],n
+            break;
+          case 7:
+            Z7Map.get(z7_args[y_arg]).invoke();
+            break;
+        }
         break;
       case 1:
         if ((y_arg == 6) && (z_arg == 6)) {
@@ -617,7 +740,56 @@ public class CPU {
         AluMap.get(alu_args[y_arg]).invoke(r_args[z_arg]);
         return M_CYCLE;
       case 3:
-        // Opcodes
+        switch(z_arg) {
+          case 0:
+            // RET cc[0],nn
+            // RET cc[1],nn
+            // RET cc[2],nn
+            // RET cc[3],nn
+            // LD (0xFF00+n),A
+            // ADD SP,d
+            // LD A,(0xFF00+n)
+            // LD HL,SP+ d
+            break;
+          case 1:
+            // Q = 0  POP rp2[p]
+            // Q = 1, 
+              // P0 = RET
+              // P1 = RETI
+              // P2 = JP HL
+              // P3 = LD SP, HL
+            break;
+          case 2:
+            // JP cc[0],nn
+            // JP cc[1],nn
+            // JP cc[2],nn
+            // JP cc[3],nn
+            // LD (0xFF00+C),A
+            // LD (nn),A
+            // LD A,(0xFF00+C)
+            // LD A,(nn)
+            break;
+          case 3:
+            // y=0 JP
+            // CB?
+            // n/a, n/a, n/a, n/a
+            // DI
+            // EI
+            break;
+          case 4:
+            // Call cc[y], nn
+            break;
+          case 5:
+            // Q = 0  PUSH rp2[p]
+            // Q = 1, Call nn
+            break;
+          case 6:
+            AluImMap.get(alu_args[y_arg]).invoke();
+            break;
+          case 7:
+            // RST y*8
+            break;
+        }
         break;
       default:    // Undefined
         System.out.println("Opcode is undefined!");
@@ -632,18 +804,30 @@ public class CPU {
    * @return
    */
   int executeExtendedOpcode() {
+    short opcode = 0;
+
     //short opcode = ReadMemory(m_ProgramCounter) ;
     //m_ProgramCounter++;
 
-    short opcode = 0;
+    int x_arg = (opcode & X_MASK) >> 6;
+    int y_arg = (opcode & Y_MASK) >> 3;
+    int z_arg = opcode & Z_MASK;
 
-    switch(opcode) {
-      case 0xB :
-        //CPU_RRC(m_RegisterDE.lo) ;
+    switch(x_arg) {
+      case 0 :
+        RotMap.get(rot_args[y_arg]).invoke(r_args[z_arg]);
         return M_CYCLE*2;
 
-      case 0x73 :
-        //CPU_TEST_BIT( m_RegisterDE.lo, 6 ) ;
+      case 1 :
+        BIT(y_arg, r_args[z_arg]);
+        return M_CYCLE*2;
+
+      case 2:
+        RES(y_arg, r_args[z_arg]);
+        return M_CYCLE*2;
+
+      case 3:
+        SET(y_arg, r_args[z_arg]);
         return M_CYCLE*2;
 
       default:
